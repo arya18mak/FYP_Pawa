23d9118dda6b46851ab488fb5700bb97ea56a6ff
arya18mak <55435847+arya18mak@users.noreply.github.com>
2021-07-22 16:59:29 +0530

    Delete Module_2/Calculating curve number using SCSN method directory

--- Module_2/Calculating curve number using SCSN method/cn_number_game.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-import random
-
-df1 = pd.read_csv("cn.csv")
-df1['S'] = df1.apply(lambda row: (1000/row.CN)-10, axis=1)
-df1['runoff'] = df1.apply(lambda row: ((row.Total_rainfall - 0.2*row.S)**2)/(row.Total_rainfall + 0.8*row.S), axis=1)
-print(df1)
-df1.to_csv("cn.csv")
